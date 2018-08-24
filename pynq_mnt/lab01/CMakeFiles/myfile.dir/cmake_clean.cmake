7fa9340fc283467c3931d894c50e5124e9c6b1c3
Xilinx User <clint.frandsen@gmail.com>
2018-08-24 23:57:12 +0000

    rename labs to lab02

---
+++ pynq_mnt/lab01/CMakeFiles/myfile.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/myfile.dir/myfile.c.o"
+  "myfile.pdb"
+  "myfile"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/myfile.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
