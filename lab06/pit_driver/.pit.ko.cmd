d7dd3b8d3acc4a0cce6642f867e20f2ca31f8628
Xilinx User <clint.frandsen@gmail.com>
2018-09-10 23:31:41 +0000

    .

---
+++ lab06/pit_driver/.pit.ko.cmd
@@ -0,0 +1,1 @@
+cmd_/home/xilinx/ECEN_427/lab06/pit_driver/pit.ko := ld -EL -r  -T ./scripts/module-common.lds --build-id  -o /home/xilinx/ECEN_427/lab06/pit_driver/pit.ko /home/xilinx/ECEN_427/lab06/pit_driver/pit.o /home/xilinx/ECEN_427/lab06/pit_driver/pit.mod.o ;  true
---
+++ lab06/pit_driver/.tmp_versions/pit.mod
@@ -0,0 +1,3 @@
+/home/xilinx/ECEN_427/lab06/pit_driver/pit.ko
+/home/xilinx/ECEN_427/lab06/pit_driver/pit.o
+
