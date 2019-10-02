9d381dce2ba641e89e2ee3a9b1dcbbb1443eabae
Clint Frandsen <clint.frandsen@gmail.com>
2019-10-02 13:08:24 -0600

    Update score_board.c

--- lab03/image_render/score_board/score_board.c
+++ lab03/image_render/score_board/score_board.c
@@ -6,13 +6,32 @@
 
 /********************************** macros ***********************************/
 #define TOP_TEN_SCORES 10
+#define PIXEL_SPACING 3
+#define ONE_PIXEL 3
+#define DEFAULT_CHAR_SCALE 3
+#define SCORE_BOARD_START_POS 333*ONE_PIXEL
 
 player_stats high_scores[TOP_TEN_SCORES];
+
+void init_score_board(){
+  for(int i = 0; i < TOP_TEN_SCORES; i++){
+    high_scores[i].score = 0;
+    high_scores[i].score_char[0] = number0_5x5;
+    high_scores[i].score_char[1] = number0_5x5;
+    high_scores[i].score_char[2] = number0_5x5;
+    
+    high_scores[i].name_char[0] = letterA_5x5;
+    high_scores[i].name_char[1] = letterA_5x5;
+    high_scores[i].name_char[2] = letterA_5x5;
+  }
+}
 
 // function to print the top 10 high scores
 void print_high_scores(){
   for(int i = 0; i < TOP_TEN_SCORES; i++){
-    sprites_render_image(saucer_16x7,16,7,0,1,white);
+    sprites_render_image(high_scores[i].name_char[0],5,5,(SCORE_BOARD_START_POS+(SCALE_NEXT_LINE*i)+(0*DEFAULT_CHAR_SCALE*ONE_PIXEL)),DEFAULT_CHAR_SCALE,white);
+    sprites_render_image(high_scores[i].name_char[1],5,5,(SCORE_BOARD_START_POS+(SCALE_NEXT_LINE*i)+(1*DEFAULT_CHAR_SCALE*ONE_PIXEL)),DEFAULT_CHAR_SCALE,white);
+    sprites_render_image(high_scores[i].name_char[2],5,5,(SCORE_BOARD_START_POS+(SCALE_NEXT_LINE*i)+(2*DEFAULT_CHAR_SCALE*ONE_PIXEL)),DEFAULT_CHAR_SCALE,white);
   }
 }
 
